4835c1525c8c81673ebcb35f1f51f743b6e76c7f
ZhovtenkoA <zhowtenko.oleksiy@gmail.com>
2024-10-13 14:53:04 +0300

    Update auth.py

--- cor_pass/routes/auth.py
+++ cor_pass/routes/auth.py
@@ -135,11 +135,16 @@ async def login(
     )
     refresh_token = await auth_service.create_refresh_token(data={"oid": user.cor_id})
     await repository_person.update_token(user, refresh_token, db)
+    is_admin = False
+    if user.email in settings.admin_accounts:
+        is_admin = True
     logger.info("login success")
+    logger.info(f"is_admin - {is_admin}")
     return {
         "access_token": access_token,
         "refresh_token": refresh_token,
         "token_type": "bearer",
+        "is_admin": is_admin,
     }
 
 
