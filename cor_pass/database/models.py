769401338a6ef797fa73875614263f1a276e8a6f
ZhovtenkoA <zhowtenko.oleksiy@gmail.com>
2024-10-17 15:21:32 +0300

    alembic + last password change
    add alembic support
    add last_password_change column to db

--- cor_pass/database/models.py
+++ cor_pass/database/models.py
@@ -33,6 +33,7 @@ class User(Base):
     email = Column(String(250), unique=True, nullable=False)
     backup_email = Column(String(250), unique=True, nullable=True)
     password = Column(String(250), nullable=False)
+    last_password_change = Column(DateTime, server_default=func.now(), onupdate=func.now())
     access_token = Column(String(250), nullable=True)
     refresh_token = Column(String(250), nullable=True)
     recovery_code = Column(
