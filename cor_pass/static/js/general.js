e84470ca08330d49bcafcbb89b7d95b9a4362b65
ZhovtenkoA <zhowtenko.oleksiy@gmail.com>
2025-06-09 16:57:34 +0300

    static update

--- cor_pass/static/js/general.js
+++ cor_pass/static/js/general.js
@@ -1,4 +1,4 @@
-// const PATIENT_COR_ID = "1GSVPC5PX-2000M";
+const PATIENT_COR_ID = "1GSVPC5PX-2000M";
 const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvaWQiOiJkYTFhNGYwNy0yODI0LTQyMWEtYmY0OC00NjhiOWQ4ZGVmYjEiLCJjb3JpZCI6IjE1MzM0OFROMS0xOTk0TSIsInJvbGVzIjpbImFkbWluIiwibGF3eWVyIiwiZG9jdG9yIiwiYWN0aXZlX3VzZXIiXSwiaWF0IjoxNzQ3MjE0ODE0LCJleHAiOjUzNDcyMTQ4MTQsInNjcCI6ImFjY2Vzc190b2tlbiJ9.IZrSncxvOpq1rZQCYFVxPhya1bSV7GIWungEYWn6_BU";
 const API_BASE_URL = "https://dev-corid.cor-medical.ua";
 
@@ -12,12 +12,4 @@ const getItemCountByAllKeys = (entity, keys = []) => {
         currentLevel = nextLevel;
         return counts;
     }, {});
-}
-
-
-const showTextareaButton = (textareaId) => {
-    const textareaNODE = document.querySelector(`#${textareaId}`);
-    textareaNODE.addEventListener('focus', () => {
-        textareaNODE.nextElementSibling.style.display = "block";
-    })
 }
