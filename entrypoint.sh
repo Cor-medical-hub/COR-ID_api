dd4a409a7a6ddd9dffec9ec7827a313b5c03b9f4
m.maliuga@cor-software.com <m.maliuga@cor-software.com>
2025-06-29 16:24:34 +0000

    Update entrypoint to mount SAMBA share

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# read secrets
+# read secrets for SAMBA
+. ./corid_env.$APP_ENV
 
-#if [[ $CORID_ENV == "prod" ]]; then
-#  . ./corid_env.production
-#fi
-#if [[ $CORID_ENV == "dev" ]]; then
-#  . ./corid_env.development
-#fi
-#
-#[[ ! -d $SCAN_DIR ]] &&  mkdir $SCAN_DIR
-#chmod -R 0777 $SCAN_DIR
-#mount -t cifs "$SCAN_UNC/$SCAN_UNC_DIR" $SCAN_DIR -o ro,user=$SCAN_USER,password=$SCAN_PASSWORD
+[[ ! -d $SCAN_DIR ]] &&  mkdir $SCAN_DIR
+chmod -R 0777 $SCAN_DIR
+mount -t cifs "$SCAN_UNC/$SCAN_UNC_DIR" $SCAN_DIR -o rw,user=$SCAN_USER,password=$SCAN_PASSWORD
 
+# just file with the timestamp
+touch $SCAN_DIR/$(date +%Y%m%d%H%M).file
 
-
+# upgrade DB
 /usr/local/bin/alembic upgrade head
 
 /usr/local/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --log-config log_config.yaml
